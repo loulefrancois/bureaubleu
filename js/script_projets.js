7248799626e6c9fac53f12b47488992c4c6c26a2
Lou Lefrançois <lefrancois.lou@gmail.com>
2023-08-29 16:49:02 +0200

    c

--- js/script_projets.js
+++ js/script_projets.js
@@ -6,10 +6,6 @@ $("#apropos").load("../header-description.html");
 
 $("#apropos").load("header-description.html");
 
-jQuery(window).load(function () {
-    jQuery(".loader").fadeOut("200");
-});
-
 // $("li").mouseover(function () {
 //     $("li").not(this).css("opacity", "0.2");
 //     $("li img").css("opacity", "0.2");
